f56c9f7fd39fce20b2a792d9d8946ddec8a0b0df
shnoble <shnoble79@gmail.com>
2019-02-18 01:34:58 +0900

    Implement simple navigation

--- app/src/main/java/com/daya/navigation/sample/MainActivity.kt
+++ app/src/main/java/com/daya/navigation/sample/MainActivity.kt
@@ -2,11 +2,17 @@ package com.daya.navigation.sample
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_blank1.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return Navigation.findNavController(this, R.id.nav_host_fragment).navigateUp()
     }
 }
